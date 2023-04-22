d8c4567c063851aab25814dbf7ffef9b32329674
oscargabriel <ozambranopinto@gmail.com>
2023-04-22 17:34:17 -0500

    añadiendo funcionalidad para crear usuarios

--- src/main/java/com/DesguaceExpress/main/services/Impl/ServiceDesguaceImpl.java
+++ src/main/java/com/DesguaceExpress/main/services/Impl/ServiceDesguaceImpl.java
@@ -1,8 +1,11 @@
 package com.DesguaceExpress.main.services.Impl;
 
+import com.DesguaceExpress.main.entities.Members;
 import com.DesguaceExpress.main.repositories.dao.Impl.RepositoryPostgreImpl;
 import com.DesguaceExpress.main.repositories.dao.RepositoryDesguace;
+import com.DesguaceExpress.main.repositories.jpa.MembersRepository;
 import com.DesguaceExpress.main.services.ServiceDesguace;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,7 +13,16 @@ public class ServiceDesguaceImpl implements ServiceDesguace {
 
     RepositoryDesguace repositoryDesguace;
 
+    @Autowired
+    MembersRepository membersRepository;
+
     public ServiceDesguaceImpl(RepositoryPostgreImpl repositoryDesguace) {
         this.repositoryDesguace = repositoryDesguace;
+    }
+
+    @Override
+    public String crearSocio(Members members) {
+        membersRepository.save(members);
+        return "creacion exitosa";
     }
 }
--- src/main/java/com/DesguaceExpress/main/services/ServiceDesguace.java
+++ src/main/java/com/DesguaceExpress/main/services/ServiceDesguace.java
@@ -1,4 +1,8 @@
 package com.DesguaceExpress.main.services;
 
+import com.DesguaceExpress.main.entities.Members;
+
 public interface ServiceDesguace {
+
+    public String crearSocio(Members members);
 }
