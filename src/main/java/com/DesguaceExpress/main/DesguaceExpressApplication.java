2e6cd2a908da2e921e5c10dc5d48cda54770aa71
oscargabriel <ozambranopinto@gmail.com>
2023-04-28 15:30:17 -0500

    prueba

--- src/main/java/com/DesguaceExpress/main/DesguaceExpressApplication.java
+++ src/main/java/com/DesguaceExpress/main/DesguaceExpressApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DesguaceExpressApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DesguaceExpressApplication.class, args);
 
